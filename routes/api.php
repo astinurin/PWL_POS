8c52a9afa3fec9790cc910dfc3984b96629b6638
astinurin <astihidayanti20@gmail.com>
2024-05-06 08:25:20 +0700

    Praktikum 3 JS 10

--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,10 @@ use App\Http\Controllers\Api\LoginController;
 // });
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class);
