239a23d84dacf6b51c72bfc0801c31b534a886a5
astinurin <astihidayanti20@gmail.com>
2024-05-06 08:57:45 +0700

    Praktikum 4 JS 10

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +29,30 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class);
+
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
+// // USER
+// Route::get('users', [UserController::class, 'index']);
+// Route::post('users', [UserController::class, 'store']);
+// Route::get('users/{user}', [UserController::class, 'show']);
+// Route::put('users/{user}', [UserController::class, 'update']);
+// Route::delete('users/{user}', [UserController::class, 'destroy']);
+
+// // KATEGORI
+// Route::get('kategori', [KategoriController::class, 'index']);
+// Route::post('kategori', [KategoriController::class, 'store']);
+// Route::get('kategori/{kategori}', [KategoriController::class, 'show']);
+// Route::put('kategori/{kategori}', [KategoriController::class, 'update']);
+// Route::delete('kategori/{kategori}', [KategoriController::class, 'destroy']);
+
+// // BARANG
+// Route::get('barang', [BarangController::class, 'index']);
+// Route::post('barang', [BarangController::class, 'store']);
+// Route::get('barang/{barang}', [BarangController::class, 'show']);
+// Route::put('barang/{barang}', [BarangController::class, 'update']);
+// Route::delete('barang/{barang}', [BarangController::class, 'destroy']);
