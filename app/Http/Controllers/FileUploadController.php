0b760dde56d3aca30bb05f1a678fd47e66d424ee
astinurin <astihidayanti20@gmail.com>
2024-05-16 07:52:49 +0700

    JS 12 C & D

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -11,35 +11,20 @@ class FileUploadController extends Controller
         return view('file-upload');
     }
 
-    // public function prosesFileUpload(Request $request){
-    //     dump($request->berkas);
-    //     // dump($request->file('file'));
-    //     // return "Pemrosesan file upload di sini";
-    // }
-
     public function prosesFileUpload(Request $request){
         // dump($request->berkas);
         // dump($request->file('file'));
         // return "Pemrosesan file upload di sini";
 
-        if($request->hasFile('berkas')) {
-            echo "path(): ".$request->berkas->path();
-            echo "<br>";
-            echo "extension(): ".$request->berkas->extension();
-            echo "<br>";
-            echo "getClientOriginalExtension(): ".
-            $request->berkas->getClientOriginalExtension();
-            echo "<br>";
-            echo "getMimeType(): ".$request->berkas->getMimeType();
-            echo "<br>";
-            echo "getClientOriginalName(): ".
-            $request->berkas->getClientOriginalName();
-            echo "<br>";
-            echo "getSize(): ".$request->berkas->getSize();
-        }
-        else{
-            echo "Tidak ada berkas yang diupload";
-        }
+        $request->validate([
+            'berkas'=>'required|file|image|max:500',]);
+            // mengambil nama file asal dengan perintah $request->berkas>getClientOriginalName()
+            // $namaFile = $request->berkas->getClientOriginalName();
+            $extFile=$request->berkas->getClientOriginalName();
+            $namaFile = 'web-' .time(). ".".$extFile;
+            $path = $request->berkas->storeAs('uploads', $namaFile);
+            echo "proses upload berhasil, file berada di : $path";
+            // echo $request->berkas->getClientOriginalName()."lolos validasi";
     }
 
 }
